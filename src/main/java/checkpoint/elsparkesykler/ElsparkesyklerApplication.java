9831563aa28667b6689db575434d7309136e954f
eirinko <eirinkorvald.ek@gmail.com>
2025-09-04 09:00:04 +0200

    Started the project

---
+++ src/main/java/checkpoint/elsparkesykler/ElsparkesyklerApplication.java
@@ -0,0 +1,13 @@
+package checkpoint.elsparkesykler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsparkesyklerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElsparkesyklerApplication.class, args);
+    }
+
+}
